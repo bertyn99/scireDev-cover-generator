174730e7cdff88b3cbdc595fb6a445c3d95b0c30
Bertyn Boulikou <bboulikou@gmail.com>
2023-03-01 11:36:43 +0100

    feat(section): add Assets section

--- components/AssetsEditor.vue
+++ components/AssetsEditor.vue
@@ -3,5 +3,6 @@
 <template>
   <div class="m-2">
     <h3 class="font-bold text-2xl text-gray-700">Assets</h3>
+    <IconPicker />
   </div>
 </template>
---
+++ components/Icon/picker.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="">
+    <BasicInput
+      label="Icons"
+      modelValue=""
+      type="text"
+      placeholder="Placeholder"
+    />
+    <div class="h-72 w-4/5 shadow-md mx-auto"></div>
+  </div>
+</template>
