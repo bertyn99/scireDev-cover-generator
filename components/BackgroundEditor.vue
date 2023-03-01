3dab273715c61208afa679d813a779fe3283ab49
Bertyn Boulikou <bboulikou@gmail.com>
2023-03-01 11:36:00 +0100

    feat(section): add Background Editor section

--- components/BackgroundEditor.vue
+++ components/BackgroundEditor.vue
@@ -1,7 +1,40 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { colorChoice } from "~~/types/color";
+
+const colors: colorChoice[] = [
+  {
+    name: "pearl",
+    color: ["#F5F5F5", "#E8E8E8"],
+  },
+  {
+    name: "candy",
+    color: ["red", "pink"],
+  },
+];
+
+const bgSelected = ref<colorChoice>(colors[0]);
+const selectBgColor = (color: string) => {
+  return colors.find((c) => c.name === color);
+};
+</script>
 
 <template>
   <div class="m-2">
     <h3 class="font-bold text-2xl text-gray-700">Background</h3>
+    <label
+      for="email"
+      class="block text-sm font-medium text-gray-700 capitalize"
+      >colors</label
+    >
+    <div class="flex flex-wrap gap-2">
+      <BasicColorCircle
+        v-for="color in colors"
+        :key="color.name"
+        :color="color.color"
+        :selected="bgSelected.name === color.name"
+        :name="color.name"
+        @clik="() => selectBgColor(color.name)"
+      />
+    </div>
   </div>
 </template>
---
+++ components/basic/colorCircle.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  color: string[];
+  name: string;
+  selected: boolean;
+}>();
+</script>
+
+<template>
+  <div
+    class="h-9 w-9 rounded-full cursor-pointer shadow-md"
+    :class="{
+      'border-2 border-blue-500': selected,
+    }"
+    :style="`background-image: linear-gradient(to right, ${color[0]} 0%, ${color[1]} 100%);`"
+  ></div>
+</template>
