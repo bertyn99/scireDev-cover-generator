2c94b34a909fe35862761abd57c95e81b0daea73
Bertyn Boulikou <bboulikou@gmail.com>
2023-03-15 22:27:07 +0100

    feat(canva): use canvasStore propety

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,65 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useCanvasStore } from "~~/store";
+
+const canvaStore = useCanvasStore();
+const height = computed(() => canvaStore.height);
+const width = computed(() => canvaStore.width);
+const coverRef = ref<HTMLCanvasElement | null>(null);
+const parentRef = ref<HTMLDivElement | null>(null);
+const aspectRatio = computed(() => {
+  return canvaStore.width / canvaStore.height;
+});
+const ctx = computed(() => coverRef.value?.getContext("2d"));
+
+watch(
+  () => canvaStore.height,
+  (val) => {
+    if (
+      parentRef.value!.offsetWidth - 32 / aspectRatio.value <
+      parentRef.value!.offsetHeight - 32
+    ) {
+      coverRef.value!.height =
+        parentRef.value!.offsetWidth - 32 / aspectRatio.value;
+    } else {
+      coverRef.value!.height = parentRef.value!.offsetHeight - 32;
+    }
+  }
+);
+watch(
+  () => canvaStore.width,
+  (val) => {
+    if (
+      parentRef.value!.offsetWidth - 32 / aspectRatio.value <
+      parentRef.value!.offsetHeight - 32
+    ) {
+      coverRef.value!.width = parentRef.value!.offsetWidth - 32;
+    } else {
+      coverRef.value!.width =
+        (parentRef.value!.offsetHeight - 32) * aspectRatio.value;
+    }
+  }
+);
+
+onMounted(() => {
+  console.log(parentRef.value!.clientWidth);
+  console.log(parentRef.value!.offsetWidth);
+});
+</script>
 
 <template>
   <SectionSidebar />
-  <main class="flex-1 p-8 flex flex-col items-center gap-8">
+  <main
+    class="flex-1 p-8 flex flex-col items-center gap-8 sm:sticky md:top-10 md:w-3/5"
+    ref="parentRef"
+  >
     <h1 class="font-bold text-3xl text-gray-800">Cover Generator</h1>
 
-    <canvas class="bg-white w-4/5 h-[400px] rounded-md"></canvas>
+    <canvas
+      class="bg-white min-h-[340px] rounded-md"
+      ref="coverRef"
+      height=""
+      width=""
+    ></canvas>
 
     <div class="flex gap-4">
       <button
