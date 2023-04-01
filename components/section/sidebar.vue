8880f5278a907f5691bfef9168ae23415435d3d6
bertyn99 <bboulikou@gmail.com>
2023-04-01 16:07:53 +0200

    hotfix: sidebar padding

--- components/section/sidebar.vue
+++ components/section/sidebar.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <aside class="w-full md:w-2/5 md:max-w-xl p-8 bg-blue-50 flex-1">
+  <aside
+    class="w-full md:w-2/5 md:max-w-xl p-2 sm:p-4 md:p-7 bg-blue-50 flex-1"
+  >
     <h2 class="font-bold text-3xl text-gray-800">Parameters</h2>
     <dl>
       <Dimension />
